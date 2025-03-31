00ecb274dcd67d1af74505125e2ec9e4a5b87ab7
gopichand-duriseti <gopiduriseti@gmail.com>
2025-03-31 19:21:34 +0530

    Update hospital_patient_management.py

--- hospital_patient_management.py
+++ hospital_patient_management.py
@@ -1,3 +1,8 @@
+patients = [
+    {"Name": "Alice", "Age": 30, "Disease": "Flu"},
+    {"Name": "Bob", "Age": 45, "Disease": "Diabetes"},
+    {"Name": "Charlie", "Age": 35, "Disease": "Flu"}]
+
 class Patient:
     def __init__(self,patients,disease):
         self.patients=patients
