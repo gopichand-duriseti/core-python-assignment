9049d3a6df6bd18e2ee7718fbb9bbe28bcfffe55
gopichand-duriseti <gopiduriseti@gmail.com>
2025-03-31 19:44:43 +0530

    Update ecommerce_cart.py

--- ecommerce_cart.py
+++ ecommerce_cart.py
@@ -1,10 +1,10 @@
 def valsum(d):
     if not d:
 	print("cart is empty")
-	break
-    if len(d)<5:
-        print(sum(d.values()))
+	return
+    if len(d)<=5:
+        print(f'Total Price:{sum(d.values())}')
     else:
-        print(sum(d.values())*0.9)
+        print(f'Total Price:{sum(d.values())*0.9}')
         
 s=valsum({'Laptop': 50000, 'Headphones': 2000, 'Mouse': 500, 'Keyboard': 1500,'Comp':30000})
